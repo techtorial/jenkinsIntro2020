8eb1e405b2daaac78004e1095d3e680378848f54
techtorial <info@techtorialacademy.com>
2020-01-11 16:22:23 -0600

    Jenkins config

---
+++ src/test/java/com/techtorial/FirstTest.java
@@ -0,0 +1,12 @@
+package com.techtorial;
+
+import org.junit.Test;
+
+public class FirstTest {
+
+
+    @Test
+    public void secondTest() {
+        System.out.println("test from first test class");
+    }
+}
